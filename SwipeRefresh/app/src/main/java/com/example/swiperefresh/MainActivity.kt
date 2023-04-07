083f6169fdae9a635ed4af81672066c09aa082e1
Rafal Mi <rafal.m4@gmail.com>
2023-04-07 11:54:19 +0200

    In SwipeRefresh, migrated from Accompanist

--- SwipeRefresh/app/src/main/java/com/example/swiperefresh/MainActivity.kt
+++ SwipeRefresh/app/src/main/java/com/example/swiperefresh/MainActivity.kt
@@ -3,16 +3,22 @@ package com.example.swiperefresh
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.pullRefresh
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -26,29 +32,59 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SwipeRefreshTheme {
-                val viewModel = viewModel<MainViewModel>()
-                val isLoading by viewModel.isLoading.collectAsState()
-                val swipeRefreshState = rememberSwipeRefreshState(isRefreshing = isLoading)
-
-                SwipeRefresh(
-                    state = swipeRefreshState,
-                    onRefresh = viewModel::loadStuff
-                ) {
-                    LazyColumn(
-                        modifier = Modifier.fillMaxSize()
-                    ) {
-                        items(100) {
-                            Text(
-                                text = "Item $it",
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(32.dp)
-                            )
-                        }
-                    }
-                }
+                // ContentWithAccompanist()
+                ContentMigrated()
             }
         }
     }
 }
 
+@Composable
+fun ContentWithAccompanist() {
+    val viewModel = viewModel<MainViewModel>()
+    val isLoading by viewModel.isLoading.collectAsState()
+    val swipeRefreshState = rememberSwipeRefreshState(isRefreshing = isLoading)
+
+    SwipeRefresh(
+        state = swipeRefreshState,
+        onRefresh = viewModel::loadStuff
+    ) {
+        TheList()
+    }
+
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun ContentMigrated() {
+    val viewModel = viewModel<MainViewModel>()
+    val isLoading by viewModel.isLoading.collectAsState()
+    val pullRefreshState = rememberPullRefreshState(
+        refreshing = isLoading,
+        onRefresh = viewModel::loadStuff
+    )
+
+    Box(modifier = Modifier.pullRefresh(pullRefreshState)) {
+        TheList()
+        PullRefreshIndicator(
+            modifier = Modifier.align(Alignment.TopCenter),
+            refreshing = isLoading,
+            state = pullRefreshState
+        )
+    }
+}
+
+@Composable
+fun TheList() {
+    LazyColumn(modifier = Modifier.fillMaxSize()) {
+        items(100) {
+            Text(
+                text = "Item $it",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(32.dp)
+            )
+        }
+    }
+
+}
